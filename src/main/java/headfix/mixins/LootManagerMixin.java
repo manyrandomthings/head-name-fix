69c9da3b733b813b61a2e3eee6d0736527c45fa7
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2023-06-08 19:37:18 -0400

    Update to 1.20

--- src/main/java/headfix/mixins/LootManagerMixin.java
+++ src/main/java/headfix/mixins/LootManagerMixin.java
@@ -1,23 +1,23 @@
 package headfix.mixins;
 
-import com.google.gson.JsonObject;
 import headfix.HeadFix;
 import net.minecraft.loot.LootManager;
 import net.minecraft.resource.ResourceManager;
-import net.minecraft.util.Identifier;
+import net.minecraft.resource.ResourceReloader;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 // run before fabric api loot events
 @Mixin(value = LootManager.class, priority = 999)
 public abstract class LootManagerMixin {
-    @Inject(method = "apply", at = @At("TAIL"))
-    private void apply(Map<Identifier, JsonObject> jsonMap, ResourceManager resourceManager, Profiler profiler, CallbackInfo info) {
+    @Inject(method = "reload", at = @At("TAIL"))
+    private void apply(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
         HeadFix.findHeadLootTables();
     }
 }
